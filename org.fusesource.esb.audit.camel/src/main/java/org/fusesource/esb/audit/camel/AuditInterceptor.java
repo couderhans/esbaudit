b05ffdc62aaa54838163a6b34899a4ed6802e6be
Hans <hans.couder@gmail.com>
2009-10-19 20:42:07 +0200

    Added node in the exchange for each step

--- org.fusesource.esb.audit.camel/src/main/java/org/fusesource/esb/audit/camel/AuditInterceptor.java
+++ org.fusesource.esb.audit.camel/src/main/java/org/fusesource/esb/audit/camel/AuditInterceptor.java
@@ -17,6 +17,7 @@
 
 package org.fusesource.esb.audit.camel;
 
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.jcr.LoginException;
@@ -29,13 +30,18 @@ import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.processor.DelegateProcessor;
 import org.apache.camel.spi.Synchronization;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.jackrabbit.value.DateValue;
 import org.fusesource.esb.audit.commons.RepositoryUtils;
 
 public class AuditInterceptor extends DelegateProcessor {
 
+    private static final Log LOG = LogFactory.getLog(AuditInterceptor.class);
+
     private final AuditInterceptStrategy strategy;
     private Session session;
+    private long steps;
 
     public AuditInterceptor(AuditInterceptStrategy strategy, Processor target) {
         super(target);
@@ -44,14 +50,16 @@ public class AuditInterceptor extends DelegateProcessor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        System.out.println("Processing Exchange");
+    	LOG.info("Processing Exchange");
+
+        createActive(exchange);
+
         exchange.addOnCompletion(new Synchronization() {
 
             public void onFailure(Exchange exchange) {
                 try {
                     failure(exchange);
                 } catch (RepositoryException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -73,10 +81,22 @@ public class AuditInterceptor extends DelegateProcessor {
         super.proceed(exchange);
     }
 
+    protected void createActive(Exchange exchange) throws Exception, RepositoryException {
+        Node node = RepositoryUtils.getOrCreate(getSession().getRootNode(), "content/audit/exchanges/"
+                + exchange.getExchangeId().toString());
+        LOG.info("Node path - Active Exchange: " + node.getPath().toString());
+    	audit(exchange, node, ExchangeStatus.Active.toString());
+        Node step = RepositoryUtils.getOrCreate(getSession().getRootNode(), "content/audit/exchanges/"
+                + exchange.getExchangeId().toString() + "/step" + steps);
+
+        audit(step, ExchangeStatus.Active.toString());
+    }
+
     protected void failure(Exchange exchange) throws Exception, RepositoryException {
         Node node = RepositoryUtils.getOrCreate(getSession().getRootNode(), "content/audit/exchanges/"
                 + exchange.getExchangeId().toString());
-        System.out.println("Node path: " + node.getPath().toString());
+
+        LOG.info("Node path - FAILURE: " + node.getPath().toString());
         audit(exchange, node, ExchangeStatus.Error.toString());
         audit(exchange.getIn(), RepositoryUtils.getOrCreate(node, "in"));
         if (exchange.hasOut()) {
@@ -87,7 +107,7 @@ public class AuditInterceptor extends DelegateProcessor {
     protected void complete(Exchange exchange) throws Exception, RepositoryException {
         Node node = RepositoryUtils.getOrCreate(getSession().getRootNode(), "content/audit/exchanges/"
                 + exchange.getExchangeId().toString());
-        System.out.println("Node path: " + node.getPath().toString());
+        LOG.info("Node path - COMPLETE: " + node.getPath().toString());
         audit(exchange, node, ExchangeStatus.Done.toString());
         audit(exchange.getIn(), RepositoryUtils.getOrCreate(node, "in"));
         if (exchange.hasOut()) {
@@ -98,9 +118,23 @@ public class AuditInterceptor extends DelegateProcessor {
     private void audit(Exchange exchange, Node node, String status) throws Exception, RepositoryException {
         node.setProperty("sling:resourceType", "audit/camel/exchange");
         node.setProperty("exchangeId", exchange.getExchangeId().toString());
-        node.setProperty("created", new DateValue(new GregorianCalendar()));
-        node.setProperty("endpointId", exchange.getFromEndpoint().toString());
+        node.setProperty("created", new Date().toString());
+        node.setProperty("endpointId", exchange.getFromEndpoint().getEndpointUri().toString());
         node.setProperty("status", status);
+        if (node.hasProperty("steps")) {
+            steps = node.getProperty("steps").getValue().getLong();
+            node.setProperty("steps",steps++);
+        } else {
+            steps = 1;
+            node.setProperty("steps", steps);
+        }
+        getSession().save();
+    }
+
+    private void audit(Node step, String status) throws RepositoryException {
+        step.setProperty("sling:resourceType", "audit/camel/exchange");
+        step.setProperty("created", new Date().toString());
+        LOG.info("Creation of new step node");
         getSession().save();
     }
 
