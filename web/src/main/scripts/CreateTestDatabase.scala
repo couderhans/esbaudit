71b46045dbcfb536467203be3fe69f17aa55853e
Hans <couderha@system-HP-Compaq-8000-Elite-CMT-PC.(none)>
2011-03-09 15:45:09 +0100

    SyntaxHighlighting toegevoegd, extre css values

--- web/src/main/scripts/CreateTestDatabase.scala
+++ web/src/main/scripts/CreateTestDatabase.scala
@@ -20,10 +20,12 @@ import org.fusesource.esbaudit.backend.model._
 
 log.info("Inserting 50 flows")
 
+val BODY = "<?xml version='1.0' encoding='UTF-8'?>\\n<po xmlns='http://www.mycompany.com/schemata/PurchaseOrder/v1' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' created='2008-06-04'\\n    id='510429'>\\n    <customer externalid='CUST07030'/>\\n    <supplier id='1234'>\\n        <name>NAME SUPPLIER</name>\\n        <address>\\n            <street>SUPPLIERSTREET 292</street>\\n            <city>BRUSSELS</city>\\n            <postalCode>1190</postalCode>\\n            <country>B</country>\\n        </address>\\n    </supplier>\\n    <delivery>\\n        <date>2008-06-06</date>\\n    </delivery>\\n    <lines>\\n        <line id='001'>\\n            <article externalid='54312ABCDEF' id='05.91.6030'>\\n                <description i18n='en'>CHEESE</description>\\n                <description i18n='fr'>FROMAGE</description>\\n            </article>\\n            <quantity uom='KARTON'>10,00</quantity>\\n        </line>\\n    </lines>\\n</po>"
+
 for (i <- 1 to 10) {
 
   MongoDB().store(Flow("flow-testing-%05d".format(i),
-                       Message("some body", Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
+                       Message(BODY, Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
                        Active(),
                        Map("my-property-%05d".format(i) -> "my-value-%05d".format(i),
                            Flow.DOCUMENT_TYPE -> "Invoice"),
@@ -34,7 +36,7 @@ for (i <- 1 to 10) {
 for (i <- 11 to 20) {
 
   MongoDB().store(Flow("flow-testing-%05d".format(i),
-                       Message("some body", Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
+                       Message(BODY, Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
                        Active(),
                        Map("my-property-%05d".format(i) -> "my-value-%05d".format(i),
                            Flow.DOCUMENT_TYPE -> "Invoice"),
@@ -45,7 +47,7 @@ for (i <- 11 to 20) {
 for (i <- 21 to 30) {
 
   MongoDB().store(Flow("flow-testing-%05d".format(i),
-                       Message("some body", Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
+                       Message(BODY, Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
                        Active(),
                        Map("my-property-%05d".format(i) -> "my-value-%05d".format(i),
                            Flow.DOCUMENT_TYPE -> "Invoice"),
@@ -56,7 +58,7 @@ for (i <- 21 to 30) {
 for (i <- 31 to 40) {
 
   MongoDB().store(Flow("flow-testing-%05d".format(i),
-                       Message("some body", Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
+                       Message(BODY, Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
                        Active(),
                        Map("my-property-%05d".format(i) -> "my-value-%05d".format(i),
                            Flow.DOCUMENT_TYPE -> "Invoice"),
@@ -67,7 +69,7 @@ for (i <- 31 to 40) {
 for (i <- 41 to 50) {
 
   MongoDB().store(Flow("flow-testing-%05d".format(i),
-                       Message("some body", Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
+                       Message(BODY, Map("my-header-%05d".format(i) -> "my-value-%05d".format(i))),
                        Active(),
                        Map("my-property-%05d".format(i) -> "my-value-%05d".format(i),
                            Flow.DOCUMENT_TYPE -> "Invoice"),
